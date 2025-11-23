7f18e92c5af35dcc0c277a2935b5092e86ba30cc
Otaiki1 <otaikisadiq2804@gmail.com>
2025-11-23 12:27:06 +0100

    :fix build issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,12 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    // Handle commonjs modules
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
   }
 })
 
